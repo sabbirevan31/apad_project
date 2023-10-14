a0ac983e8f5bda95e8eab0c50c016c049aef6482
Sabbirvan31 <evan.cnits@gmail.com>
2023-10-14 18:46:45 +0600

    activity section ongoing

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,21 @@ $("#slider").owlCarousel({
     },
   },
 });
+
+$("#slider1").owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
